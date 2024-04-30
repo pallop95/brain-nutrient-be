d330fb463a0ff9e2571854417cf556d121aecee1
pallop95 <pallop.bunnak@gmail.com>
2024-04-30 15:32:53 +0700

    swagger generate json

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 import { AppService } from './app.service';
 
 @Controller()
+@ApiTags('Swagger')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('json')
+  @ApiResponse({ status: 200, description: 'Swagger JSON file' })
+  async getJson(@Res() res: Response): Promise<void> {
+    const filePath = resolve(__dirname, '../openAPI/swagger.json');
+    const content = readFileSync(filePath, 'utf8');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(content);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
+import { resolve } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +15,17 @@ async function bootstrap() {
     .build();
   // TODO: can't pass 'Bearer <Token>', check later...
   const document = SwaggerModule.createDocument(app, options);
+
+  // Save the Swagger JSON document to a file
+  const filePath = resolve(__dirname, '../openAPI/swagger.json');
+  try {
+    console.log(__dirname);
+    writeFileSync(filePath, JSON.stringify(document, null, 2));
+    console.log('Swagger JSON file generated successfully.');
+  } catch (error) {
+    console.error('Error writing Swagger JSON file:', error);
+  }
+
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
