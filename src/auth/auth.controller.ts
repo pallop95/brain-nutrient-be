9cced86d09342b05b22c01b8b74c87849f34c5c5
pallop95 <pallop.bunnak@gmail.com>
2024-04-30 16:08:06 +0700

    added promised output

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,7 @@ import { Public } from 'src/decorator/public.decorator';
 import { LoginDto } from './dto/login.dto';
 import { RefreshTokenDto } from './dto/refresh-token.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { AccessToken } from './types/accessToken';
 
 @Controller('auth')
 @ApiTags('AuthController')
@@ -14,7 +15,7 @@ export class AuthController {
   @Public() // Public decorator allows access without authentication
   @Post('login')
   @HttpCode(HttpStatus.OK)
-  async login(@Body() loginDto: LoginDto) {
+  async login(@Body() loginDto: LoginDto): Promise<AccessToken> {
     return this.authService.login(loginDto);
   }
   // signIn(@Body() signInDto: Record<string, any>) {
@@ -24,13 +25,15 @@ export class AuthController {
   @Public() // Public decorator allows access without authentication
   @Post('register')
   @HttpCode(HttpStatus.OK)
-  async register(@Body() loginDto: LoginDto) {
+  async register(@Body() loginDto: LoginDto): Promise<AccessToken> {
     return this.authService.register(loginDto);
   }
 
   @Public()
   @Post('refresh-token')
-  async refreshToken(@Body() refreshTokenDto: RefreshTokenDto) {
+  async refreshToken(
+    @Body() refreshTokenDto: RefreshTokenDto,
+  ): Promise<AccessToken> {
     return this.authService.refreshToken(refreshTokenDto);
   }
 
